40df85ef98be6804e2e3e6b50faf8dabec9d2e5e
cghiee <cghiee@rit.edu>
2020-06-29 11:42:11 -0400

    new gains

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <math.h>       /* pow */
 
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
@@ -12,19 +13,36 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  this->Kp=Kp_;
+  this->Ki=Ki_;
+  this->Kd=Kd_;
+
+  this->p_error=0;
+  this->i_error=0;
+  this->d_error=0;
 
 }
 
 void PID::UpdateError(double cte) {
+
   /**
    * TODO: Update PID errors based on cte.
    */
+  this->d_error=cte-this->p_error;  
+  this->i_error+=cte;
+  this->p_error=cte;  
 
 }
 
-double PID::TotalError() {
+double PID::TotalError(double speed) {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double d_limit=0.15;
+  double limited_d_term=0;
+          if (Kd*d_error>d_limit) limited_d_term=d_limit;
+          if (Kd*d_error<-d_limit) limited_d_term=-d_limit;
+
+  //return Kp*p_error*(speed/pow(speed+.1,0.5)) + Ki*i_error + Kd*d_error*(speed/15);  // TODO: Add your total error calc here!
+  return Kp*p_error + Ki*i_error + limited_d_term;  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "json.hpp"
 #include "PID.h"
+#include <math.h>       
 
 // for convenience
 using nlohmann::json;
@@ -30,13 +31,35 @@ string hasData(string s) {
   return "";
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   uWS::Hub h;
 
   PID pid;
+
+  double init_Kp=atof(argv[1]);
+  double init_Ki=atof(argv[2]);
+  double init_Kd=atof(argv[3]);
+
+  std::cout << "Kp: " << init_Kp << " Ki: " << init_Ki <<
+          " Kd " << init_Kd << std::endl;
   /**
    * TODO: Initialize the pid variable.
    */
+  pid.Init(init_Kp,init_Ki,init_Kd);
+
+
+ /**********
+
+
+ WORKING PARAMETERS
+  ./pid -0.225 0 -10
+
+
+  ****/
+
+
+  /* end TODO */
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -63,16 +86,28 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+
+          pid.UpdateError(cte);
+         
+
+          steer_value=pid.TotalError(speed);
+          //steer_value=-cte;
+          // limit steering angle
+          double limit=0.75;
+          if (steer_value>limit) steer_value=limit;
+          if (steer_value<-limit) steer_value=-limit;
+
+          /* end TODO */
           
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value <<
+          " Speed: " << speed << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          // FOR DEBUG std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
       } else {
